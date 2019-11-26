42cc88ee297efcb7bf273087332f1714a75bc5d6
DucTrongVo <duc-trong.vo@toulouse.miage.fr>
2019-11-26 14:42:08 +0100

    fix #1 Tests sur QuestionAChoixExclusif

--- src/test/java/miagem1/QuestionAChoixExclusifTest.java
+++ src/test/java/miagem1/QuestionAChoixExclusifTest.java
@@ -7,15 +7,36 @@ import static org.junit.Assert.*;
 
 public class QuestionAChoixExclusifTest {
 
+  private QuestionAChoixExclusif uneQuestion;
   @Before
   public void setUp() throws Exception {
+    // given : un objet de type QuestionAChoixExclusif
+    uneQuestion = new QuestionAChoixExclusif("un énoncé",2);
   }
 
   @Test
-  public void getEnonce() {
+  public void testgetEnonce() {
+    // when : on demande l'énoncé à la question
+    String resEnonce = uneQuestion.getEnonce();
+    // then : l'énoncé est non nul
+    assertNotNull(resEnonce);
+    // and : l'énoncé est bien celui fourni à la construction
+    assertEquals(resEnonce,"un énoncé");
   }
 
   @Test
-  public void getScoreForIndice() {
+  public void testgetScoreForIndice() {
+    // when : un étudiant fourni l'indice correspondant à la bonne réponse
+    int indiceEtudiant = 2;
+    // and: on demande le calcule du score
+    Float resScore = uneQuestion.getScoreForIndice(indiceEtudiant);
+    // then : le score obtenu est 100
+    assertEquals(new Float(100f),resScore);
+    // when : un étudiant fourni l'indice correspondant à une mauvais réponse
+    indiceEtudiant = 1;
+    // and: on demande le calcule du score
+    resScore = uneQuestion.getScoreForIndice(indiceEtudiant);
+    // then : le score obtenu est 0
+    assertEquals(new Float(0f),resScore);
   }
 }
